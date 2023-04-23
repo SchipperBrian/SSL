9bb7eec53cf96d68236f3c99729dd875d44d43a7
Brian Schipper <bschipper@frba.utn.edu.ar>
2023-04-23 18:44:19 -0300

    Creación del ejecutable.exe y archivo output

--- 00-CHelloWorld/hello.c
+++ 00-CHelloWorld/hello.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main() {
-   printf("Hello, World!");
-   return 0;
+
+    FILE *file_excercise0 = fopen("output.txt", "w");
+
+    if (file_excercise0 == NULL)
+    {  
+        printf("Error in file excercise0 \n");
+        //exit(1);
+    }
+
+    fprintf(file_excercise0, " Hello world! "); 
+
+    return 0;
 }
